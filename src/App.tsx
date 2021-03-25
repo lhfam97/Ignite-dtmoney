1903eb476760039d26eca8b1ba44a9861886e82f
Luis Henrique Fontes Alcantara Machado <luis.machado@cencosud.com.br>
2021-03-25 13:19:52 -0300

    Adicao do componente Summary

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import "./App.css";
-import styled from "styled-components";
+
 import { Header } from "./components/Header";
+import { Dashboard } from "./components/Dashboard";
 import { GlobalStyle } from "./styles/global";
-// const Title = styled.h1`
-//   font-size: 64px;
-//   color: #8257e6;
-// `;
+
+
 
 export function App() {
   return (
     <>
       <Header />
+      <Dashboard/>
       <GlobalStyle />
     </>
   );
